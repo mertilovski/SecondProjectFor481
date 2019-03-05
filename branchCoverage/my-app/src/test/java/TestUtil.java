574a689a1474ad019de646289366707f083ce208
mertilovski <mertkaya@etu.edu.tr>
2019-03-05 21:31:33 +0300

    TestUtil added tests : one,even

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -13,4 +14,27 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+
+  public void TestOneLength(){
+
+    int[] arr = {1};
+    boolean result = c.compute(arr);
+    assertEquals(true,result == false);
+
+  }
+
+  @Test
+  public void TestEvenLength(){
+
+    int[] arr = {1,2};
+    boolean result = c.compute(arr);
+    assertEquals(true,result == false);
+
+  }
+
+
+
+
 }
